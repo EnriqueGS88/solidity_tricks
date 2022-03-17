d39f92515722c172749158f1d93777792851491a
EnriqueGS88 <enrique.gzs@gmail.com>
2022-03-17 23:23:54 +0100

    verifySignature and setup hardhat dependencies

---
+++ snippets/signatureVerification.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+
+/** Signature Verification
+
+How to Sign and Verify Messages
+
+
+ */
+
+contract VerifySignature {
+    function getMessageHash(
+        address _to, uint _amount, string memory _message, uint _nonce
+    )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Source: https://www.youtube.com/watch?v=NP4db_UPVwc
